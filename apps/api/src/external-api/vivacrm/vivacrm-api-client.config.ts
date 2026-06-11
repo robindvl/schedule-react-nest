15a5c2627d0acd71016c5500df42404853238635
robindvl <120911231+robindvl@users.noreply.github.com>
2026-06-11 13:45:08 +0300

    Enhance VivaCRM API client configuration to include custom headers
    - Added custom headers to the API client configuration to address 403 errors from VivaCRM WAF.
    - Set 'Accept' to 'application/json' and specified a custom 'User-Agent' for requests.

--- apps/api/src/external-api/vivacrm/vivacrm-api-client.config.ts
+++ apps/api/src/external-api/vivacrm/vivacrm-api-client.config.ts
@@ -3,4 +3,9 @@ import type { CreateClientConfig } from './@generated/client.gen';
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
   baseUrl: 'https://api.vivacrm.ru/end-user/api/v1/iSkq6G',
+  headers: {
+    ...config?.headers,
+    Accept: 'application/json',
+    'User-Agent': 'schedule-react-nest/1.0',
+  },
 });
